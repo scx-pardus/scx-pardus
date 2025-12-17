363a616e9282f51e8bb78de64832cfc8d04db0b4
udstbtk <deniztubitak@gmail.com>
2025-12-18 01:27:13 +0300

    Removed prebuilt binaries for ort

--- scheds/rust/scx_pardus/build.rs
+++ scheds/rust/scx_pardus/build.rs
@@ -4,6 +4,36 @@
 // GNU General Public License version 2.
 
 fn main() {
+//println!("cargo:rustc-link-search=native=/home/user2/build_on/MinSizeRel/_deps/abseil_cpp-build/absl/container");
+
+    
+  //  println!("cargo:rustc-link-search=native=/home/user2/build_on/MinSizeRel");
+
+      /*println!("cargo:rustc-link-lib=static=onnxruntime_session");
+    println!("cargo:rustc-link-lib=static=onnxruntime_framework");
+    println!("cargo:rustc-link-lib=static=onnxruntime_common");
+    println!("cargo:rustc-link-lib=static=onnxruntime_test_utils");*/
+
+
+
+
+    println!("cargo:rustc-link-lib=static=absl_hashtablez_sampler");
+
+/*
+    println!("cargo:rustc-link-lib=static=absl_raw_hash_set");
+    println!("cargo:rustc-link-lib=static=absl_hash");
+    println!("cargo:rustc-link-lib=static=absl_city");
+    println!("cargo:rustc-link-lib=static=absl_low_level_hash");
+    println!("cargo:rustc-link-lib=static=absl_base");
+    println!("cargo:rustc-link-lib=static=absl_spinlock_wait");
+    println!("cargo:rustc-link-lib=static=absl_malloc_internal");
+
+   
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+    println!("cargo:rustc-link-lib=dylib=pthread");
+    println!("cargo:rustc-link-lib=dylib=dl");
+    println!("cargo:rustc-link-lib=dylib=m");
+    println!("cargo:rustc-link-lib=dylib=rt");*/
     scx_cargo::BpfBuilder::new()
         .unwrap()
         .enable_intf("src/bpf/intf.h", "bpf_intf.rs")
